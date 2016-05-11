c4b6c90107ea57489285d58ec053e2798960fa54
DiSalvio <stephenjamesdisalvio@gmail.com>
2016-05-10 20:08:46 -0400

    update seeds, add back accidentally deleted admin method

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,8 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, and :omniauthable
   devise :timeoutable, :database_authenticatable, :lockable, :trackable
+
+  def self.set_admin
+    @admin = Admin.first
+  end
 end
